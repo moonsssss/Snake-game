b75d20019cd8a60461d92f2d396d0650228003b0
Lochlann O'Connell <loceanstaff@gmail.com>
2023-03-12 16:45:20 -0700

    Added Background and Apple image

--- Snake/snake game.py
+++ Snake/snake game.py
@@ -1,42 +1,20 @@
-# import pygame
-# #from sys import exit
-
-# pygame.init()
-# screen = pygame.display.set_mode((800, 400))
-
-# pygame.display.set_caption("Snake 101")
-
-# background = pygame.image.load("Snake\images\Snake Game background.jpg")
-
-# while True: 
-#    for event in pygame.event.get():  
-#        if event.type == pygame.QUIT:
-#            pygame.quit()
-#            #exit()
-#     #screen.blit(background, (0,0))
-
-#    pygame.display.update()  
-
-
-
-"""
-Ulo Freitas pygame starter. Creating a blank screen.
-"""
 import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
-# creating a name for your game
-pygame.display.set_caption("Ulo's Super Cool Game!!")
+screen = pygame.display.set_mode((548, 483))
 
-while True:
-   # Event loop (checking for player input)
-   for event in pygame.event.get():
-       # gets all of the "events" in pygame
+pygame.display.set_caption("Snake 101")
+
+background = pygame.image.load("Snake\images\Snake Game background.jpg")
+apple = pygame.image.load("Snake\images\—Pngtree—fruit cartoon fruit cartoon red_3806661.png").convert_alpha()
+apple_rec = apple.get_rect(midbottom = (328, 325))
+while True: 
+   for event in pygame.event.get():  
        if event.type == pygame.QUIT:
            pygame.quit()
            exit()
-
-   # Draw all of our elements! (like the draw function in trinket)
-   pygame.display.update()  # This will update the screen to the player!
+    
+   screen.blit(background, (0,0))
+   screen.blit(apple, apple_rec)
+   pygame.display.update()  
