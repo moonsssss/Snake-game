8dccb7b912ec60ad35af4802c2b96581d1112182
Lochlann O'Connell <loceanstaff@gmail.com>
2023-03-12 17:31:08 -0700

    made apple move to the left

--- Snake/snake game.py
+++ Snake/snake game.py
@@ -1,13 +1,15 @@
 import pygame
+import pygame.locals
 from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((548, 483))
 
 pygame.display.set_caption("Snake 101")
-
+clock = pygame.time.Clock()
 background = pygame.image.load("Snake\images\Snake Game background.jpg")
-apple = pygame.image.load("Snake\images\—Pngtree—fruit cartoon fruit cartoon red_3806661.png").convert_alpha()
+apple = pygame.image.load("Snake\images\Apple.png").convert_alpha()
+apple = pygame.transform.scale(apple, (32, 32))
 apple_rec = apple.get_rect(midbottom = (328, 325))
 while True: 
    for event in pygame.event.get():  
@@ -17,4 +19,9 @@ while True:
     
    screen.blit(background, (0,0))
    screen.blit(apple, apple_rec)
-   pygame.display.update()  
+   print(apple_rec.x)
+   keys = pygame.key.get_pressed()
+   if keys[pygame.K_LEFT]:
+    apple_rec.x = apple_rec.x - 1
+   pygame.display.update()
+   clock.tick(60)
